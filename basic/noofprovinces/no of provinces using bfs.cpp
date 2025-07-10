e342c5e8151c984e83f39ac3c6ba91a290127bbb
varsha7104 <130915408+varsha7104@users.noreply.github.com>
2025-07-10 15:47:28 +0530

    Update no of provinces using bfs.cpp

--- basic/noofprovinces/no of provinces using bfs.cpp
+++ basic/noofprovinces/no of provinces using bfs.cpp
@@ -4,35 +4,47 @@ Difficulty: MediumAccuracy: 54.29%Submissions: 153K+Points: 4Average Time: 20m
 Given an undirected graph with V vertices. We say two vertices u and v belong to a single province if there is a path from u to v or v to u. Your task is to find the number of provinces.
 
 Note: A province is a group of directly or indirectly connected cities and no other cities outside of the group.*/
+// User function Template for C++
+
 class Solution {
   public:
-  void bfs(int u,vector<int>adj[],vector<bool>&vis,queue<int>&q){
+  void bfs(int u,vector<vector<int>>&adj,vector<bool>&vis){
+      queue<int>q;
       vis[u]=true;
-      for(int &v:adj[u]){
-          if(!vis[v])
-          {
-              q.push(v);
-              bfs(v,adj,vis,q);
+      q.push(u);
+      while(!q.empty()){
+          int u=q.front();
+          q.pop();
+          for(int &v:adj[u]){
+              if(!vis[v]){
+                  vis[v]=true;
+                  q.push(v);
+              }
           }
       }
   }
+ 
     int numProvinces(vector<vector<int>> adj, int V) {
-        vector<int>adjls[V];
-       for(int i=0;i<adj.size();i++){
-           for(int j=0;j<adj.size();j++){
-               if(i!=j && adj[i][j]!=0){
-                   adjls[i].push_back(j);
-                   adjls[j].push_back(i);
-               }
-           }
-       }int n=V;
-       vector<bool>vis(n,false);
-       queue<int>q;int c=0;
-       for(int i=0;i<n;i++){
-           if(!vis[i]) {c++;bfs(i,adjls,vis,q);}
-       }
-        return c;
+        // code here
+        vector<vector<int>>adjls(V);
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                if(i!=j && adj[i][j]!=0){
+                    adjls[i].push_back(j);
+                    adjls[j].push_back(i);
+                }
+            }
+        }vector<bool>vis(V,false);
+        int c=0;
+      for(int i=0;i<V;i++){
+          if(!vis[i]) { bfs(i,adjls,vis);c++;}
+      }
+       
+       
+       
+       return c;
     }
+};
 };/*Input:[[1, 0, 1],[0, 1, 0],[1, 0, 1]]
  
 Output:
